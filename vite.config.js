ba35dda43c3ad5a16c3f8d2f68c905a4aa31266a
DavidaaWoW <davidik.gegiya@gmail.com>
2023-05-13 13:51:14 +0300

    add https

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: { https: true },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
             ssr: 'resources/js/ssr.js',
             refresh: true,
-            server: { https: true },
         }),
         react(),
     ],
